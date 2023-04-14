9a653f14485530672505a7fc63c1d7205adfbae3
jellyjw <wldnzzz@naver.com>
2023-04-14 22:29:42 +0900

    feat: dropdown

--- src/components/elements/Navbar/Navbar.tsx
+++ src/components/elements/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <div className="w-full flex items-center justify-around bg-indigo-500 text-white h-20">
       {navItems?.map((el, idx) => (
-        <>
+        <div key={idx}>
           <Link href={el.link}>
-            <div key={idx}>{el.text}</div>
+            <div>{el.text}</div>
           </Link>
-        </>
+        </div>
       ))}
     </div>
   );
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../elements/Navbar/Navbar";
+import Dropdown from "../Dropdown";
 
 export default function MainLayout() {
   return (
     <main className="h-full w-full">
       <Navbar />
+      <Dropdown />
     </main>
   );
 }
