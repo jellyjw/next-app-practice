19c3b0e2f64362a54b3b90bf0e29768c5dcc869c
jellyjw <wldnzzz@naver.com>
2023-04-16 20:42:09 +0900

    feat: Card 컴포넌트

---
+++ src/components/elements/Card/Card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return (
+    <article className="bg-white-300 w-80 h-[300px] rounded-xl shadow shadow-gray-500/50 cursor-pointer">
+      image,text
+    </article>
+  );
+}
